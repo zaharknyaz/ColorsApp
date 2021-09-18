e4d63079fae2bc826deeaf2ad83551ba25a7bfed
Zahar Knyazev <knyazzz1989@mail.ru>
2021-09-18 20:58:37 +0400

    Initial Commit

---
+++ Colors_zaharknyazev_18092021/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Colors_zaharknyazev_18092021
+//
+//  Created by Захар Князев on 18.09.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Colors_zaharknyazev_18092021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Colors_zaharknyazev_18092021
+//
+//  Created by Захар Князев on 18.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
